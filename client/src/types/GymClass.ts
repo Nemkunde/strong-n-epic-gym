198cd5a24926bd248ce33b174c12b098924f4d17
Nemkunde <nemkunde.liljemark@student.kyh.se>
2023-09-11 13:09:08 +0200

    changes

--- client/src/types/GymClass.ts
+++
@@ -1,8 +0,0 @@
-export interface GymClass {
-    title: string; 
-    coach: string; 
-    day: string, 
-    date: string; 
-    time: string; 
-    description: string; 
-}
--- client/src/types/Users.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    id: Number, 
-    username: string, 
-    role: string, 
-    password: string, 
-    activities: [], 
-}
